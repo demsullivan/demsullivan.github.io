f90d98c5e9008d213748f5a33a8422ce11605e06
Dave Sullivan <dave@dave-sullivan.com>
2015-01-01 11:04:49 -0500

    big updates to index page

--- js/template.js
+++ js/template.js
@@ -321,6 +321,7 @@
 			$("[data-animation-effect]").each(function() {
 				var $this = $(this),
 				animationEffect = $this.attr("data-animation-effect");
+			        accY = parseInt($this.attr("data-acc-y")) || -130;
 				if(Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
 					$this.appear(function() {
 						var delay = ($this.attr("data-effect-delay") ? $this.attr("data-effect-delay") : 1);
@@ -328,7 +329,7 @@
 						setTimeout(function() {
 							$this.addClass('animated object-visible ' + animationEffect);
 						}, delay);
-					}, {accX: 0, accY: -130});
+					}, {accX: 0, accY: accY});
 				} else {
 					$this.addClass('object-visible');
 				}
